a6530a8aaa3997c414bb3e5603f83b93cc4984b3
Sandro Braun <supermario94123@gmail.com>
2020-04-27 08:45:02 +0200

    adds pytorch view layer

---
+++ supermariopy/ptutils/layers.py
@@ -0,0 +1,21 @@
+import torch
+
+
+class View(torch.nn.Module):
+    def __init__(self, *shape):
+        """Reshape layer.
+
+        https://discuss.pytorch.org/t/equivalent-of-np-reshape-in-pytorch/144/5
+        
+        Examples
+        --------
+
+        ```python
+            sequential_model = torch.nn.Sequential([Linear(10, 20), View(-1, 5, 4)])
+        ```
+        """
+        super(View, self).__init__()
+        self.shape = shape
+
+    def forward(self, input):
+        return input.view(*self.shape)
