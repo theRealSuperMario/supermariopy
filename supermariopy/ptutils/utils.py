de1926838482dac853174814e5701593a9bddf53
Sandro Braun <supermario94123@gmail.com>
2020-04-27 08:48:48 +0200

    adds pytorch utils

--- supermariopy/ptutils/utils.py
+++ supermariopy/ptutils/utils.py
@@ -18,13 +18,29 @@ def to_torch(x, permute=False):
     """automatically convert numpy array to torch and permute channels from NHWC to NCHW"""
     if isinstance(x, np.ndarray):
         x = torch.from_numpy(x)
-    if torch.cuda.is_available():
-        device = torch.device("cuda")
-    else:
-        device = torch.device("cpu")
-    x = x.to(device)
+        if torch.cuda.is_available():
+            device = torch.device("cuda")
+        else:
+            device = torch.device("cpu")
+        x = x.to(device)
 
     if permute:
         x = x.permute((0, 3, 1, 2))  # NHWC --> NCHW
+    if x.dtype is torch.float64:
+        x = x.type(torch.float32)
     return x
 
+
+def split_stack(x, split_sizes, split_dim, stack_dim):
+    """Split x along dimension split_dim and stack again at dimension stack_dim"""
+    t = torch.stack(torch.split(x, split_sizes, dim=split_dim), dim=stack_dim)
+    return t
+
+
+def split_stack_reshape(x, split_sizes=3):
+    """split x at dimension 1, stack them again and reshape into batch axis."""
+    t = split_stack(x, split_sizes, split_dim=1, stack_dim=1)
+    shape_ = list(x.shape)
+    shape_[0] = -1
+    shape_[1] = split_sizes
+    return t.view(shape_)
