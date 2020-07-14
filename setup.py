97d6eacf639f77f15b4e57552ed19ea759c89838
Sandro Braun <sandrobraun@Sandros-MacBook-Pro.local>
2020-07-14 17:31:44 +0200

    version update

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="supermariopy",
-    version="0.1",
-    description="python library, cripts and notebooks that are usfull from time to time",
+    version="0.2",
     url="https://github.com/theRealSuperMario/supermariopy",
     author="Sandro Braun",
     author_email="supermario94123@gmail.com",
