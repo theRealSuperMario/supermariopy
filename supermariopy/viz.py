800d70a6b747a217edd3f213f49ffbb22703b7d6
therealsupermario <supermario94123@gmail.com>
2020-08-25 15:18:53 +0200

    refactors viz

--- supermariopy/viz.py
+++ supermariopy/viz.py
@@ -1,12 +1,12 @@
-from matplotlib import pyplot as plt
 import numpy as np
-from supermariopy import imageutils
-from supermariopy import numpyutils as npu
+from matplotlib import pyplot as plt
+
+from . import imageutils
 
 
 def argmax_rgb(m, cmap=plt.cm.viridis):
     """Take argmax of m along dimension 1 and apply RGB colorcode on it
-    
+
     Parameters
     ----------
     m : np.ndarray
@@ -19,13 +19,13 @@ def argmax_rgb(m, cmap=plt.cm.viridis):
 
     See also
     --------
-    .. tfutils.viz.argmax_rgb 
+    .. tfutils.viz.argmax_rgb
     """
     B, H, W, P = m.shape
     argmax_map = np.argmax(m, axis=-1)
     colors = imageutils.make_colors(P, cmap=cmap)
     colors = colors.astype(np.float32)
-    m_one_hot = npu.one_hot(argmax_map, P, axis=-1)
+    m_one_hot = imageutils.one_hot(argmax_map, P, axis=-1)
     mask_rgb = np.einsum("bhwp,pc->bhwc", m_one_hot, colors)
     return mask_rgb
 
@@ -38,19 +38,23 @@ def show_active_parts(
     cmap=plt.cm.viridis,
 ):
     """Highlight active parts in color and set color of other parts to a gray value
-    
+
     Parameters
     ----------
     parts : np.ndarray
-        tensor of binary masks indicating mutually exclusive parts or segments of a segmentation.
+        tensor of binary masks indicating mutually exclusive parts or segments of a
+        segmentation.
         Shaped [N, H, W, P]
     active_idx : np.array or list
-        list of indices indicating which part (indexing into last dimension of parts) are active.
+        list of indices indicating which part (indexing into last dimension of parts)
+        are active.
     inactive_parts_color : np.ndarray, default np.array([0.8, 0.8, 0.8])
         Color value in range [0, 1] for inactive parts.
     background_color : np.ndarray, default np.array([1.0, 1.0, 1.0])
-        Color value in range [0, 1] for background. Note that this only makes sense if parts does not provide a background itself.
-        If part contains a background part, it will be assigned a background_color specified by the color map.
+        Color value in range [0, 1] for background. Note that this only makes sense if
+        parts does not provide a background itself.
+        If part contains a background part, it will be assigned a background color
+        specified by the color map.
     """
     if not any([isinstance(active_idx, list), isinstance(active_idx, np.ndarray)]):
         raise TypeError()
@@ -65,6 +69,3 @@ def show_active_parts(
     parts = np.insert(parts, 0, background_part, axis=-1)
     parts_rgb = np.einsum("nhwp,pc->nhwc", parts, colors)
     return parts_rgb
-
-
-# TODO segmentation false color plot
